09bef758ed2ca06c8715e2c058e2352667f83c61
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-07-16 11:41:33 +0200

    working game

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -1,12 +1,12 @@
 import Relay from 'react-relay';
 
+/*
+ * anchor our game to the game root field of the schema:
+ */
 export default class extends Relay.Route {
+  static path = '/';
   static queries = {
-    viewer: () => Relay.QL`
-      query {
-        viewer
-      }
-    `,
+    game: () => Relay.QL`query { game }`,
   };
   static routeName = 'AppHomeRoute';
 }
